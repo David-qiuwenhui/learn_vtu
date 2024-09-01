439bb1559f27c2727b75c342f1792d8751fd6651
qiuwenhui <1059713136@qq.com>
2024-09-01 15:59:33 +0800

    🧪 test: init test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,18 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
+    // given
     const msg = 'new message'
+
+    // when
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+      propsData: { msg }
     })
+
+    console.log(wrapper.text())
+    console.log(wrapper.html())
+
+    // then
     expect(wrapper.text()).toMatch(msg)
   })
 })
